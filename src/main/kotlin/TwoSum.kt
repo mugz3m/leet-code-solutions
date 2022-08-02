2647ba853017d8f88fd4882cda0077be64fc24d0
Maxim Sergeev <mugz3m.others@gmail.com>
2022-08-02 21:33:59 +0300

    Solve TwoSum
    https://leetcode.com/problems/two-sum/

---
+++ src/main/kotlin/TwoSum.kt
@@ -0,0 +1,13 @@
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val result = IntArray(2)
+    for (i in nums.indices) {
+        for (j in i + 1 until nums.size) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i
+                result[1] = j
+                break
+            }
+        }
+    }
+    return result
+}
