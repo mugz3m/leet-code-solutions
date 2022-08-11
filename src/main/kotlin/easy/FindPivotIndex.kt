1f9c5c56fccb3f9449c5600c82e3995ea50e1810
Maxim Sergeev <mugz3m.others@gmail.com>
2022-08-11 22:34:09 +0300

    Solve Find Pivot Index
    https://leetcode.com/problems/find-pivot-index/

---
+++ src/main/kotlin/easy/FindPivotIndex.kt
@@ -0,0 +1,12 @@
+package easy
+
+fun pivotIndex(nums: IntArray): Int {
+    var sum = 0
+    var leftsum = 0
+    for (x in nums) sum += x
+    for (i in nums.indices) {
+        if (leftsum == sum - leftsum - nums[i]) return i
+        leftsum += nums[i]
+    }
+    return -1
+}
