16cdf1f25788a42262d557aaf23ab2a0653bf01c
Maxim Sergeev <mugz3m.others@gmail.com>
2022-08-16 22:04:05 +0300

    Resolve Reverse Linked List
    https://leetcode.com/problems/reverse-linked-list/

--- src/main/kotlin/easy/ReverseLinkedList.kt
+++ src/main/kotlin/easy/ReverseLinkedList.kt
@@ -2,6 +2,13 @@ package easy
 
 import easy.auxiliary.ListNode
 
-private class ListNode(var nodeValue: Int) {
-    var next: ListNode? = null
+fun reverseList(head: ListNode?): ListNode? {
+    return reverseListInt(head, null)
+}
+
+private fun reverseListInt(head: ListNode?, newHead: ListNode?): ListNode? {
+    if (head == null) return newHead
+    val next = head.next
+    head.next = newHead
+    return reverseListInt(next, head)
 }
