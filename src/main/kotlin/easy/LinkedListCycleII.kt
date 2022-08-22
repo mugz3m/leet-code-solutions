d02d30c240dbd047636fdda50b2836289af166ec
Maxim Sergeev <mugz3m.others@gmail.com>
2022-08-22 15:59:39 +0300

    Solve Linked List Cycle II
    https://leetcode.com/problems/linked-list-cycle-ii/

---
+++ src/main/kotlin/easy/LinkedListCycleII.kt
@@ -0,0 +1,24 @@
+package easy
+
+import easy.auxiliary.ListNode
+
+fun detectCycle(head: ListNode?): ListNode? {
+    if (head?.next == null) return null
+
+    var fast = head
+    var slow = head
+    var entry = head
+
+    while (fast!!.next != null && fast.next!!.next != null) {
+        slow = slow!!.next
+        fast = fast.next!!.next
+        if (slow == fast) {
+            while (slow != entry) {
+                slow = slow!!.next
+                entry = entry!!.next
+            }
+            return entry
+        }
+    }
+    return null
+}
