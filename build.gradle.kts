67dd238da640e20d402183f330dc9ccf9b4288c9
Maxim Sergeev <mugz3m.others@gmail.com>
2023-07-10 23:40:09 +0300

    Migrate to Kotlin DSL. Update Gradle and Kotlin versions

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+}
+
+group = "io.mugz3m"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+kotlin {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_11)
+}
