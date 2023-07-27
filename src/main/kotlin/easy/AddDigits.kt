f6e6f0850ffd72551bfe28e6c1ccf16b5f724cd6
Maxim Sergeev <mugz3m.others@gmail.com>
2023-07-27 23:52:27 +0300

    Update Add Digits solution
    https://leetcode.com/problems/add-digits/

--- src/main/kotlin/easy/AddDigits.kt
+++ src/main/kotlin/easy/AddDigits.kt
@@ -1,10 +1,9 @@
 package easy
 
 fun addDigits(num: Int): Int {
-    var result = num
-    while (result / 10 != 0) {
-        result = result / 10 + result % 10
+    return when {
+        num == 0 -> 0
+        num % 9 == 0 -> 9
+        else -> num % 9
     }
-
-    return result
 }
